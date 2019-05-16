7b6735f542a551a347689670bd763f51aa9b388b
Extrogenesis <extrogenesis@gmail.com>
2019-05-16 13:48:51 -0400

    mod ga.js

--- Unnamed Site 2/www/ga.js
+++ Unnamed Site 2/www/ga.js
@@ -1,8 +1,8 @@
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-35209631-1']);
-_gaq.push(['_trackPageview']);
 
 (function () {
+    _gaq.push(['_setAccount', 'UA-35209631-1']);
+    _gaq.push(['_trackPageview']);
     var ga = document.createElement('script');
     ga.type = 'text/javascript';
     ga.async = true;
@@ -13,11 +13,11 @@ _gaq.push(['_trackPageview']);
 })();
 
 (function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
-     fjs.parentNode.insertBefore(js, fjs);
-}
-(document, 'script', 'facebook-jssdk'));
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+        fjs.parentNode.insertBefore(js, fjs);
+    }
+    (document, 'script', 'facebook-jssdk'));
