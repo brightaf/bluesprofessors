cf8eda6784e6ea8ab8725427d58afb843d4a52c0
Extrogenesis <extrogenesis@gmail.com>
2019-05-25 22:21:11 -0400

    refactored ajax call

--- js/image.js
+++ js/image.js
@@ -1,12 +1,21 @@
 $.ajax({
     type: 'GET',
-    url: "img",
-    dataType: 'json',
+    url: "img/content.json",
+    // dataType: 'application/json',
     contentType: 'application/json',
-    success: function (data) {
-        for (let index = 0; index < data.length; index++) {
-            $(".wrapper").append("<img class= gallery src='" + "img\/" + data[index] + "'>");
-            
+    success: function (imageDirectory) {
+        for (var index = 0; index < imageDirectory.images.length; index++) {
+            $(".wrapper").append("<img class=gallery src='img/" + imageDirectory.images[index] + "'>")
         }
+
     }
 });
+
+// $(".wrapper").append("<img class=gallery src='img/" + data.images + "'>");
+
+
+// $(data).find('a').attr("href", function (i, value) {
+//     if (value.match(/\.(jpg|jpeg|png|gif)/)) {
+//         $(".wrapper").append("<img class= gallery src='" + value + "'>");
+//     }
+// });
