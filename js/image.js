ae45cc83b96c2537303c8345d181ec783c4e39c1
Extrogenesis <extrogenesis@gmail.com>
2019-05-25 22:47:59 -0400

    refactored and deleted comments

--- js/image.js
+++ js/image.js
@@ -1,8 +1,5 @@
 $.ajax({
-    // type: 'GET',
     url: "img/content.json",
-    // dataType: 'application/json',
-    // contentType: 'application/json',
     success: function (imageDirectory) {
         for (var index = 0; index < imageDirectory.images.length; index++) {
             $(".wrapper").append("<img class=gallery src='img/" + imageDirectory.images[index] + "'>")
@@ -10,12 +7,3 @@ $.ajax({
 
     }
 });
-
-// $(".wrapper").append("<img class=gallery src='img/" + data.images + "'>");
-
-
-// $(data).find('a').attr("href", function (i, value) {
-//     if (value.match(/\.(jpg|jpeg|png|gif)/)) {
-//         $(".wrapper").append("<img class= gallery src='" + value + "'>");
-//     }
-// });
